9cb231e488280db8bdd312d8ce97efe69c957ff8
Kevin Schneider <schneider.kev@outlook.de>
2023-04-20 16:42:32 +0200

    Finish basic tests

---
+++ tests/ReferenceObjects.fs
@@ -0,0 +1,33 @@
+﻿module ReferenceObjects
+
+open TestUtils
+
+open System.Text.Json.Nodes
+
+module JsonStrings = 
+
+    let string_value = getEmbeddedJsonString "test_files.string_value.json"
+
+    let number_value = getEmbeddedJsonString "test_files.number_value.json"
+
+    let boolean_value = getEmbeddedJsonString "test_files.boolean_value.json"
+
+    let array_value = getEmbeddedJsonString "test_files.array_value.json"
+
+    let object_value = getEmbeddedJsonString "test_files.object_value.json"
+
+    let all_values = getEmbeddedJsonString "test_files.all_values.json"
+
+module JsonObjects =
+    
+    let string_value = JsonObject.Parse(JsonStrings.string_value).AsObject()
+
+    let number_value = JsonObject.Parse(JsonStrings.number_value).AsObject()
+
+    let boolean_value = JsonObject.Parse(JsonStrings.boolean_value).AsObject()
+
+    let array_value = JsonObject.Parse(JsonStrings.array_value).AsObject()
+
+    let object_value = JsonObject.Parse(JsonStrings.object_value).AsObject()
+
+    let all_values = JsonObject.Parse(JsonStrings.all_values).AsObject()
---
+++ tests/TestObjects.fs
@@ -0,0 +1,67 @@
+﻿module TestObjects
+
+open JsonDSL
+
+let string_value =
+    object {
+        property "string_value" "yes"
+    }
+
+let number_value =
+    object {
+        property "number_value" 42
+    }
+
+let boolean_value =
+    object {
+        property "boolean_value" true
+    }
+
+let array_value = 
+    object {
+        property "array_value" (array {
+            yield true
+            for i in 2..8 -> i
+            yield "9"
+            yield [10]
+            yield (object {
+                property "11" 11
+            })
+        })
+    }
+
+let object_value =
+    object {
+        property "object_value" (object {
+            property "string_value" "yes"
+            property "number_value" 42
+            property "boolean_value" true
+            property "array_value" [1..8]
+        })
+    }
+
+let all_values =
+    object {
+        property "string_value" "yes"
+        property "number_value" 42
+        property "boolean_value" true
+        property "array_value" [
+            box "string"
+            box 42
+            box false
+            box [ "inner_array" ]
+        ]
+
+        property "object_value" (object {
+            property "string_value" "yes"
+            property "number_value" 42
+            property "boolean_value" true
+            property "array_value" [
+                box "string"
+                box 42
+                box false
+                box [ "inner_array" ]
+            ]
+        })
+
+    }
